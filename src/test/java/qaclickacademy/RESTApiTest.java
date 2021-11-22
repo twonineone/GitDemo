4efd81bc4cb5db81e5b1c21db2197eac0995e5ec
sonali <sonalihindesks@gmail.com>
2021-11-22 06:18:41 +0530

    This is my first commit

---
+++ src/test/java/qaclickacademy/RESTApiTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTApiTest 
+{
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira.......");
+		
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deletetwitter.......");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest 
+{
+	@Test
+	public void BrowserAutomtion()
+	{
+		System.out.println("BrowserAutomtion");
+		
+	}
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+	
+
+}
